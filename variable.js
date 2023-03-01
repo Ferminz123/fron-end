86f0d8f9ecdc844e4a4c0abbe0d41fe7e758c4f0
Ferminz123 <126729569+Ferminz123@users.noreply.github.com>
2023-03-01 20:20:01 -0300

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,34 @@
+
+
+
+
+document.getElementById('boton_').onclick = function () {
+    document.getElementById("demo").innerHTML = "Sus Datos de contacto fueron enviados";
+}
+
+document.addEventListener('click'), function() {
+
+}
+
+document.getElementById('boton_enviar').addEventListener('click', function () {
+    document.getElementById('apreta').style.display = 'none';
+})
+
+
+
+
+
+
+
+var nombre="daniel"
+
+console.log("nombre vale :" +nombre);
+
+if(asdasd) {
+    console.log("asdasdad");
+}
+else{
+
+}
+
+fo
